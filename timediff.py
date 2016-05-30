0b38dca35f285c1187c40ad9c53ab13dbef5e8b8
ryan <ryanjamesdillon@gmail.com>
2016-05-30 14:02:54 +0200

    old unknown changes to timediff.py

--- timediff.py
+++ timediff.py
@@ -1,5 +1,16 @@
 import datetime
 
+def seconds_between(date1, date2):
+	return abs((date2 - date1).days*86400.)
+
+
+def minutes_between(date1, date2):
+	return abs((date2 - date1).days*1440.)
+
+
+def hours_between(date1, date2):
+	return abs((date2 - date1).days*24.)
+
 
 def days_between(date1, date2):
 	return abs((date2 - date1).days)
@@ -23,6 +34,9 @@ def print_time_between(date1, date2):
 	print ' Date 1:', date1
 	print ' Date 2:', date2
 	print '----------------------------'
+	print 'Seconds: {:>8}'.format(str(seconds_between(date1,date2)))
+	print 'Minutes: {:>8}'.format(str(minutes_between(date1,date2)))
+	print '  Hours: {:>8}'.format(str(hours_between(date1,date2)))
 	print '   Days: {:>8}'.format(str(days_between(date1,date2)))
 	print ' Months: {:>8}'.format(str(months_between(date1,date2)))
 	print '  Years: {:>8}'.format(str(years_between(date1,date2)))
@@ -32,14 +46,16 @@ def print_time_between(date1, date2):
 
 if __name__ == '__main__':
 
-	import sys
+    import sys
 
-	# Commandline Arguments Handling #
-	if len(sys.argv) < 3:
-		print "\ndate format: yyyy-mm-dd"
-		sys.exit("usage: "+sys.argv[0]+" <date 1> <date2>\n")
+    # Commandline Arguments Handling #
+    if len(sys.argv) < 3:
+        print "\ndate format: yyyy-mm-dd"
+        sys.exit("usage: "+sys.argv[0]+" <date 1> <date2>\n")
 
-	date1 = datetime.datetime.strptime(sys.argv[1], "%Y-%m-%d")
-	date2 = datetime.datetime.strptime(sys.argv[2], "%Y-%m-%d")
+    date1 = datetime.datetime.strptime(sys.argv[1], "%Y-%m-%d")
+    date2 = datetime.datetime.strptime(sys.argv[2], "%Y-%m-%d")
 
-#print 'months:',(datetime.date(year2,month2,day2).toordinal() - datetime.date(year1,month1,day1).toordinal())/30.
+    print_time_between(date1, date2)
+
+    #print 'months:',(datetime.date(year2,month2,day2).toordinal() - datetime.date(year1,month1,day1).toordinal())/30.
